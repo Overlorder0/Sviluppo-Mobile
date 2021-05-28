0ce32bed7d364d1fcfbddad14622909fd7e81022
Over0 <creper1208@yandex.ru>
2021-05-28 19:00:47 +0300

    Практика 2 переделанная

---
+++ practice2/dialog/src/main/java/ru/mirea/bulatkin/dialog/AlertDialogFragment.java
@@ -0,0 +1,42 @@
+package ru.mirea.bulatkin.dialog;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.DialogFragment;
+
+import ru.mirea.bulatkin.dialog.R;
+
+public class AlertDialogFragment extends DialogFragment {
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Здравствуй МИРЭА!")
+                .setMessage("Служил?")
+                .setIcon(R.mipmap.ic_launcher_round)
+                .setPositiveButton("Идём дальше", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // Закрываем окно
+                        ((MainActivity)getActivity()).onOkClicked();
+                        dialog.cancel();
+                    }
+                })
+                .setNeutralButton("На паузе", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        ((MainActivity)getActivity()).onNeutralClicked();
+                        dialog.cancel();
+                    }
+                })
+                .setNegativeButton("Никак нет", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        ((MainActivity)getActivity()).onCancelClicked();
+                        dialog.cancel();
+                    }
+                });
+        return builder.create();
+    }
+}
